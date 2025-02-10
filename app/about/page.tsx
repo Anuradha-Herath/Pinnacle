0622198cb979596ed4a3dc509f0dc2d073424832
Anuradha Herath <anuradhaherath2001@gmail.com>
2025-02-10 18:00:49 +0530

    Home page with header and banner image

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to Pinnacle! We are dedicated to providing the best service possible.</p>
+            <p>Our mission is to deliver high-quality products that bring value to our customers.</p>
+        </div>
+    );
+};
+
+export default AboutPage;
